3bcca4efa03bdddd29b75cc637fb33e5472ee9e4
Shota Taniguchi <staniguchi@genvidtech.com>
2024-02-09 17:31:24 +0900

    updated favicon path in document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,11 @@ const GukouDocument = () => {
   return (
     <Html lang="ja">
       <Head>
-        <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
-        <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
-        <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-        <link href="/site.webmanifest" rel="manifest" />
-        <link color="#5bbad5" href="/safari-pinned-tab.svg" rel="mask-icon" />
+        <link href="/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+        <link href="/favicons/site.webmanifest" rel="manifest" />
+        <link color="#5bbad5" href="/favicons/safari-pinned-tab.svg" rel="mask-icon" />
         <meta content="#da532c" name="msapplication-TileColor" />
         <meta content="#ffffff" name="theme-color" />
       </Head>
