2dc5fa52bbca389148f9cbd6cb350bf078e05fc7
Shota Taniguchi <staniguchi@genvidtech.com>
2024-02-10 18:49:44 +0900

    created card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import React, { FC } from 'react';
+
+import QiitaImage from '@/public/qiita.webp';
+import ZennImage from '@/public/zenn.png';
+import { cx } from '@/src/utils/helper';
+
+import styles from './Card.module.scss'; // CSSモジュールのインポート
+
+export interface ICardProps {
+  category: string[];
+  href: string;
+  title: string;
+  type: 'Qiita' | 'Zenn'; // 'Qiita' または 'Zenn' のみを許可
+}
+
+const Card: FC<ICardProps> = ({ title, href, type }) => {
+  return (
+    <a className={styles.card} href={href} target="_blank">
+      <Image
+        alt={type}
+        className={cx(styles.logo, type === 'Qiita' && styles.qiita)}
+        height={150}
+        src={type === 'Qiita' ? QiitaImage : ZennImage}
+        width={200}
+      />
+      <div className={styles.cardBottom}>
+        <div className={styles.title}>{title}</div>
+        {/* <div className={styles.category}>
+          {category.map((cat, index) => (
+            <span key={index}>{cat}</span>
+          ))}
+        </div> */}
+      </div>
+    </a>
+  );
+};
+
+export default Card;
--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -3,12 +3,28 @@
 import { motion } from 'framer-motion';
 import { FC } from 'react';
 
+import Card, { ICardProps } from '@/src/components/card/card';
+
 import styles from './articles.module.scss';
+
+const mockData: ICardProps[] = [
+  { title: 'タイトル1', href: 'https://example.com/1', type: 'Qiita', category: ['プログラミング', 'React'] },
+  { title: 'タイトル2', href: 'https://example.com/2', type: 'Zenn', category: ['フロントエンド', 'Vue'] },
+  { title: 'タイトル1', href: 'https://example.com/1', type: 'Qiita', category: ['プログラミング', 'React'] },
+  { title: 'タイトル2', href: 'https://example.com/2', type: 'Zenn', category: ['フロントエンド', 'Vue'] },
+  { title: 'タイトル1', href: 'https://example.com/1', type: 'Qiita', category: ['プログラミング', 'React'] },
+  { title: 'タイトル2', href: 'https://example.com/2', type: 'Zenn', category: ['フロントエンド', 'Vue'] },
+  { title: 'タイトル1', href: 'https://example.com/1', type: 'Qiita', category: ['プログラミング', 'React'] },
+];
 
 const AboutPage: FC = () => {
   return (
     <motion.div animate={{ opacity: 1 }} className={styles.articles} initial={{ opacity: 0 }}>
-      <h1 className={styles.title}> this is articles page</h1>
+      <div className={styles.grid}>
+        {mockData.map((item, index) => (
+          <Card category={item.category} href={item.href} key={index} title={item.title} type={item.type} />
+        ))}
+      </div>
     </motion.div>
   );
 };
