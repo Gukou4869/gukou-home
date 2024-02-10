c545303dd0a271891bc622b0c4deb145bf6546a0
Shota Taniguchi <staniguchi@genvidtech.com>
2024-02-10 16:48:22 +0900

    added useEffect in navigation

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import { setPageState } from '@/src/store/slice/pageSlice';
-import { useAppDispatch, useAppSelector } from '@/src/store/store';
+import { useAppDispatch } from '@/src/store/store';
 import { cx } from '@/src/utils/helper';
 import { PageType } from '@/src/utils/type';
 
@@ -33,25 +34,29 @@ function capitalizeFirstLetter(text: string) {
 }
 
 const Navigation = () => {
-  const pageState = useAppSelector((state) => state.page.pageState);
   const dispatch = useAppDispatch();
   const router = useRouter();
+  const currentPath = router.asPath === '/' ? 'home' : (router.asPath.slice(1, router.asPath.length) as PageType);
 
   const handleNavigation = async (navItem: INavItem) => {
     try {
       await router.push(navItem.path);
-      dispatch(setPageState(navItem.title));
     } catch (error) {
       console.error('Failed to navigate:', error);
     }
   };
+
+  // set Current Page to redux store when refresh
+  useEffect(() => {
+    dispatch(setPageState(currentPath));
+  }, [dispatch, router.asPath, currentPath]);
 
   return (
     <nav className={styles.navigation}>
       <ul className={styles.navs}>
         {NavItems.map((item) => {
           return (
-            <li className={cx(styles.navItem, pageState === item.title && styles.active)} key={item.path}>
+            <li className={cx(styles.navItem, currentPath === item.title && styles.active)} key={item.path}>
               <button
                 type="button"
                 onClick={() => {
