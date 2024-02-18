ec432cd9c590d0eff6eb937e90f45f71010f6445
Shota Taniguchi <staniguchi@genvidtech.com>
2024-02-19 08:56:47 +0900

    update global scss

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 import { setPageState } from '@/src/store/slice/pageSlice';
-import { useAppDispatch } from '@/src/store/store';
+import { useAppDispatch, useAppSelector } from '@/src/store/store';
 import { cx } from '@/src/utils/helper';
 import { PageType } from '@/src/utils/type';
 
@@ -36,6 +36,7 @@ function capitalizeFirstLetter(text: string) {
 const Navigation = () => {
   const dispatch = useAppDispatch();
   const router = useRouter();
+  const currentMq = useAppSelector((state) => state.mq.size);
   const currentPath = router.asPath === '/' ? 'home' : (router.asPath.slice(1, router.asPath.length) as PageType);
 
   const handleNavigation = async (navItem: INavItem) => {
@@ -52,7 +53,7 @@ const Navigation = () => {
   }, [dispatch, router.asPath, currentPath]);
 
   return (
-    <nav className={styles.navigation}>
+    <nav className={styles.navigation} data-current-mq={currentMq}>
       <ul className={styles.navs}>
         {NavItems.map((item) => {
           return (
