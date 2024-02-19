ae54d6c7d9a147437e3a4f0f336b457494024ad1
Shota Taniguchi <staniguchi@genvidtech.com>
2024-02-19 15:12:08 +0900

    update to responsive design

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -5,22 +5,20 @@ import { FC } from 'react';
 import ReactImage from '@/public/react.webp';
 import TypeScriptImage from '@/public/typescript.png';
 import NameSection from '@/src/components/name/name';
-import { useAppSelector } from '@/src/store/store';
 import { BIO_TEXT, FULL_NAME, ROLE, linkItems } from '@/src/utils/constants';
 
 import styles from './about.module.scss';
 
 const AboutPage: FC = () => {
-  const mq = useAppSelector((state) => state.mq.size);
   return (
     <motion.div animate={{ opacity: 1 }} className={styles.about} initial={{ opacity: 0 }}>
       <NameSection fullName={FULL_NAME} role={ROLE} />
       <section className={styles.descriptionSection}>
         <h1>{BIO_TEXT}</h1>
       </section>
-      <section style={{ display: 'flex', gap: 30 }}>
-        <Image alt="TypeScript logo" className={styles.image} data-current-mq={mq} src={TypeScriptImage} />
-        <Image alt="React logo" className={styles.image} data-current-mq={mq} src={ReactImage} />
+      <section className={styles.imageSection}>
+        <Image alt="TypeScript logo" className={styles.image} src={TypeScriptImage} />
+        <Image alt="React logo" className={styles.image} src={ReactImage} />
       </section>
       <section className={styles.linksSection}>
         <ul className={styles.links}>
